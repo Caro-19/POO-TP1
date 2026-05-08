9525d0d1ab268a9c0185ee696fa76d1014e90c90
Carol Torrez <torrezcarol1902@gmail.com>
2026-05-08 11:01:36 -0300

    Add files via upload

---
+++ trabajo-practico-1/trabajo-practico-1/src/main/java/ar/org/centros8/java/curso/trabajo_practico_1/entidades/clientes/Cliente.java
@@ -0,0 +1,16 @@
+package ar.org.centros8.java.curso.trabajo_practico_1.entidades.clientes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+
+public abstract class Cliente{
+    private final int nroDeCliente;
+
+}
